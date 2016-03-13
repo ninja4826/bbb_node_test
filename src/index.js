7155375c3f361510c3bc4a4276769445fe721320
Russell Hueske <hueske.russ690@gmail.com>
2016-03-12 20:15:01 -0600

    ugh

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import b from 'bonescript';
+import b from 'octalbonescript';
 
 var ledOn = 0;
+var blinkInterval;
+var pin = 'USR3';
 
-b.pinMode(b.USR3, 'out');
-
-var blinkInterval = setInterval(() => {
-    ledOn = ledOn ? 0 : 1;
-    b.digitalWrite(b.USR3, ledOn);
-}, 1000);
+b.pinMode(pin, b.OUTPUT, (err1) => {
+    if (err1) {
+        console.error(err1.message);
+        return;
+    }
+    blinkInterval = setInterval(() => {
+        ledOn = ledOn ? 0 : 1;
+        b.digitalWrite(pin, b.HIGH, (err2) => {
+            if (err2) {
+                console.error(err2.message);
+                return;
+            }
+        });
+    });
+});
